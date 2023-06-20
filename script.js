e2fd4efcaa4ae05fba123ca0ca6a49e2cfcd30a0
dvcorreia1984 <victor@onsleer.co.za>
2023-06-20 08:53:01 +0200

    Added Javascript functions to open and close menu

---
+++ script.js
@@ -0,0 +1,17 @@
+function menuopen() {
+    document.getElementById("mobilenav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  function menukill() {
+    document.getElementById("mobilenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
+  function closeMenu() {
+    menukill();
+    var menuItems = document.querySelectorAll("#mobilenav a");
+    menuItems.forEach(function(item) {
+      item.removeEventListener("click", closeMenu);
+    });
+  }
